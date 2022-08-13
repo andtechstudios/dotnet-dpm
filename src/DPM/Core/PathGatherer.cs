235de03aa41899a122bb7601e62daa2331fc6676
Andrew <andrewmjordan@users.noreply.github.com>
2022-08-13 14:34:40 -0700

    improve readability

--- src/DPM/Core/PathGatherer.cs
+++ src/DPM/Core/PathGatherer.cs
@@ -36,43 +36,49 @@ namespace Andtech.DPM
 				Console.ResetColor();
 				Environment.Exit(-1);
 			}
+
 			var destinationRootsGlob = session.ClientShell.ExpandEnvironmentVariables(installLocation.Destination);
 			var destinationRoots = Glob.ExpandNames(destinationRootsGlob, dirOnly: true)
 				.ToList();
 			
 			var results = new List<Result>();
-			foreach (var destinationRoot in destinationRoots)
+
+			foreach (var include in package.include)
 			{
-				foreach (var include in package.include)
+				var sourcePaths = include.ExpandGlob(package.Root);
+				foreach (var sourcePath in sourcePaths)
 				{
-					var sourcePaths = include.ExpandGlob(package.Root);
-					foreach (var sourcePath in sourcePaths)
+					// Evaluate destinations
+					var destinationPathGlob = session.ClientShell.ExpandEnvironmentVariables(include.destination);
+					
+					if (Path.IsPathRooted(destinationPathGlob))
 					{
-						// Evaluate destinations
-						var destinationPathGlob = session.ClientShell.ExpandEnvironmentVariables(include.destination);
-						var destinationPaths = Glob.ExpandNames(GetRootedPath(destinationPathGlob));
-
-						foreach (var destinationPath in destinationPaths)
+						foreach (var destinationPath in Glob.ExpandNames(destinationPathGlob))
 						{
-							var result = new Result()
-							{
-								SourcePathFull = Path.Combine(package.Root, sourcePath),
-								SourcePath = sourcePath,
-								DestinationPath = destinationPath,
-							};
-
-							results.Add(result);
+							Add(destinationPath);
 						}
-
-						string GetRootedPath(string path)
+					}
+					else
+					{
+						foreach (var destinationRoot in destinationRoots)
 						{
-							if (!Path.IsPathRooted(path))
-							{
-								path = Path.Combine(destinationRoot, include.GetDestinationPath(sourcePath));
-							}
+							var destinationPath = Path.Combine(destinationRoot, include.GetDestinationPath(sourcePath));
 
-							return path;
+							Add(destinationPath);
 						}
+					}
+
+					// Local functions
+					void Add(string destination)
+					{
+						var result = new Result()
+						{
+							SourcePathFull = Path.Combine(package.Root, sourcePath),
+							SourcePath = sourcePath,
+							DestinationPath = destination,
+						};
+
+						results.Add(result);
 					}
 				}
 			}
